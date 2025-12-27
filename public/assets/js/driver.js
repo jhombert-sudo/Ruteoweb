f518d4881aeb2405f3c2157cbb535d4756a0a1b1
Ruteoweb <jhombert@bidcom.com.ar>
2025-12-27 20:55:34 -0300

    Add event listener for arrival button

--- public/assets/js/driver.js
+++ public/assets/js/driver.js
@@ -1,1 +1,7 @@
-// driver.js
+const btn = document.getElementById("btnLlegada");
+const estado = document.getElementById("estado");
+
+btn.addEventListener("click", () => {
+  estado.classList.remove("oculto");
+  estado.innerText = "Llegada registrada (simulación)";
+});
