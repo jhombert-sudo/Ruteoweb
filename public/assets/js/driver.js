5c44eb2e16ebea0c0fcc615a9a1eeb613b00c1ff
Ruteoweb <jhombert@bidcom.com.ar>
2025-12-27 21:11:21 -0300

    Delete public directory

--- public/assets/js/driver.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btnLlegada");
-const estado = document.getElementById("estado");
-
-btn.addEventListener("click", () => {
-  estado.classList.remove("oculto");
-  estado.innerText = "Llegada registrada (simulación)";
-});
